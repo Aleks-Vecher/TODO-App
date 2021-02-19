32b1d4f31fd4f5b4d5111d7b3edd65f8199842e5
Aleks-Vecher <aleks.vecher@gmail.com>
2021-02-19 18:03:41 +0300

    Filter component creation

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import CategoryList from './component/CategoryList';
 import TodoList from './component/TodoList';
 import AddTodo from './component/AddTodo';
 import ProgressBar from './component/ProgressBar';
+import Filter from './component/Filter';
 
 const App = () => (
   <div className="start-screen-container m-4">
     <div className="row header">
-      <div className="col-12 ">
+      <div className="col-6">
         <h1>To-Do List</h1>
+      </div>
+      <div className="col-6">
+        <Filter />
       </div>
     </div>
     <div className="row ">
